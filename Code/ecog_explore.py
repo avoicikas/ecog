48af81bdd28d24ce3d6b09c82c9ade5f0ca9b032
Aleksandras Voicikas <avoicikas@gmail.com>
2021-03-30 20:27:33 +0300

    general function

--- Code/ecog_explore.py
+++ Code/ecog_explore.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: ipynb,py:light,md:myst
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.9.1
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
 
 # # ECOG
 #
